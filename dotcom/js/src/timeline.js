e4e3ae1d44a4d53fef5b38a5cce35cc85e68f2fc
gowrishankarjallu <gowrishankar.jallu@gmail.com>
2018-04-16 15:35:24 -0400

    Contact card changes

--- dotcom/js/src/timeline.js
+++ dotcom/js/src/timeline.js
@@ -371,5 +371,61 @@ function createTimeline(name, dob, retirementDate, retirementPlan) {
 }
 
 
+
+function getAgentInfo() {
+    var perldata = getCookie('perl-custumer-care');
+    var perlCustumerData = JSON.parse(perldata);
+    console.log("this is perl-customer-dare data+" + " " + perlCustumerData);
+
+    var perlName = perlCustumerData.Name;
+    var perlZip = perlCustumerData.Zip;
+    var perlTelephone = perlCustumerData.Telephone;
+    var perlRegion = perlCustumerData.Region;
+    var perlHeader = perlCustumerData.Header;
+    var result = perlHeader.split(".");
+    
+    
+    var k = '<div class="contact">';
+    k += '<div class="content">';
+    if(result){
+        k += '<h2>' + result[0] + '</h2>';
+        k += '<p>' + result[1] + '</p>';
+        }
+        else{
+            k += '<h3>' + result + '</h3>';
+        }
+    
+    k += '<div class="card">';
+    k += '<div class="row">';
+    k += '<div class="col-sm-4">';
+    k += '<div class="contact_image">';
+    k += '<img src="../../dotcom/images/highmarkLogo.png">';
+    k += '</div>';
+    k += '</div>';
+    k += '<div class="col-sm-6">';
+    k += '<div class="info">';
+    k += '<h3>' + perlName + '</h3>';
+    k += '<label>' + "phone:" + '</label>';
+    k += '<span>' + perlTelephone + '</span>';
+    k += '<br>';
+    k += '<label>' + "email:" + '</label>';
+    
+    //    k += '<label>phone:</label>' +  '<span>' + + '</span><br>';
+    k += '</div>';
+    k += '</div>';
+    k += '</div>';
+    k += '</div>';
+    k += '</div>';
+    k += '</div>';
+   // $("#agentInfo").show();
+    $("#agentInfo1").append(k);
+    $("#agentInfo2").append(k);
+    $("#agentInfo3").append(k);
+    $("#agentInfo4").append(k);
+    $("#agentInfo5").append(k);
+}
+
+
+
      });
  })(window, document, jQuery);
